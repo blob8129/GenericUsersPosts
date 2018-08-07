4f6487a2685467f1f610280e6fe13f52f1d7fe8a
andrey-volobuev <andrey.volobuev@schibsted.com>
2018-08-07 17:35:46 +0200

    Added user entity

---
+++ UsersPosts/Users/Entity/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  UsersPosts
+//
+//  Created by Andrey Volobuev on 8/7/18.
+//  Copyright © 2018 blob8129. All rights reserved.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    //First and Last Name of the user
+    let name: String
+    let userName: String
+    let email: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+}
+
+
